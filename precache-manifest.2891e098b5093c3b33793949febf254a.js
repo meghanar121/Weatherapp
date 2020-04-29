f8012b3dca87f92b5bf1869091e83df36ed3cf9d
meghanar121 <meghanar121@gmail.com>
2020-04-29 10:40:32 +0200

    Updates

---
+++ precache-manifest.2891e098b5093c3b33793949febf254a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d70b7867e7795465ff3f36de5137ef",
+    "url": "/Weatherapp/index.html"
+  },
+  {
+    "revision": "75ea81ecbb4daec73907",
+    "url": "/Weatherapp/static/css/main.baacf8cf.chunk.css"
+  },
+  {
+    "revision": "3420651d9b88680d8ffd",
+    "url": "/Weatherapp/static/js/2.3a5dd0d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weatherapp/static/js/2.3a5dd0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ea81ecbb4daec73907",
+    "url": "/Weatherapp/static/js/main.6c7777e1.chunk.js"
+  },
+  {
+    "revision": "499fc2fc5111d3d523a7",
+    "url": "/Weatherapp/static/js/runtime-main.30efa873.js"
+  },
+  {
+    "revision": "d985936890aa28e4f7bcfd496ca7e3c8",
+    "url": "/Weatherapp/static/media/cloudy.d9859368.jpeg"
+  },
+  {
+    "revision": "d2c349772129aab8b138344a2cb0eb68",
+    "url": "/Weatherapp/static/media/sunny1.d2c34977.jpeg"
+  }
+]);
